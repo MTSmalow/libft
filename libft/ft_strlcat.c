f63916a4cbfb3611056a57aeb08d77a16261ad71
Eduardo Medeiros diniz <medeirosdinieduardo@gmail.com>
2026-05-18 16:27:45 -0300

    Add implementations of ft_strchr, ft_strlcat, and ft_strrchr functions

---
+++ libft/ft_strlcat.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edmedeir <edmedeir@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/18 14:21:18 by edmedeir          #+#    #+#             */
+/*   Updated: 2026/05/18 14:45:24 by edmedeir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<libft.h>
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	dest_len;
+	unsigned int	src_len;
+	unsigned int	i;
+
+	dest_len = 0;
+	src_len = 0;
+	i = 0;
+	while (dest[dest_len] && dest_len < size)
+		dest_len++;
+	while (src[src_len])
+		src_len++;
+	if (size <= dest_len)
+		return (size + src_len);
+	while (src[i] && (dest_len + i) < (size - 1))
+	{
+		dest[dest_len + i] = src[i];
+		i++;
+	}
+	dest[dest_len + i] = '\0';
+	return (dest_len + src_len);
+}
