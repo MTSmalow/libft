0094419e50a27f65bfd00ae271419c10019e19e1
Eduardo Medeiros diniz <medeirosdinieduardo@gmail.com>
2026-05-26 14:44:45 -0300

    feat: add ft_putchar_fd, ft_putstr_fd, and ft_strjoin implementations

---
+++ libft/ft_strjoin.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edmedeir <edmedeir@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/26 14:17:41 by edmedeir          #+#    #+#             */
+/*   Updated: 2026/05/26 14:32:45 by edmedeir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*joined;
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	if (!s1 || !s2)
+		return (0);
+	joined = (char *)malloc(sizeof (char) * ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!joined)
+		return (0);
+	while (s1[i])
+	{
+		joined[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		joined[i + j] = s2[j];
+		j++;
+	}
+	joined[i + j] = '\0';
+	return (joined);
+}
